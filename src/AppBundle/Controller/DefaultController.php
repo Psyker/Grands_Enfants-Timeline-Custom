326f7e6e556357721c5eb07676d8aed6aa500a12
Omar Jbara <omar.jbara2@gmail.com>
2016-04-27 04:23:47 +0200

    Added cookie to call the Twitter API once a day and, if there are new tweets, fetch them and put them in the databases

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,7 +5,9 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Tweet;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -14,36 +16,39 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        $tweets = $this->get('app.twitter')->get('statuses/user_timeline', [
-            'screen_name' => 'grands_enfants',
-            'exclude_replies' => true
-        ]);
-        foreach ($tweets as $tweet) {
-            $myTweet = new Tweet();
-            $myTweet->setCreatedAt(new \DateTime($tweet->created_at));
-            $myTweet->setLikes($tweet->favorite_count);
-            $myTweet->setImageUrl($tweet->entities->media[0]->media_url);
-            $mentions = [];
-            foreach ($tweet->entities->user_mentions as $mention) {
-                array_push($mentions, $mention->screen_name);
-            }
-            $myTweet->setMentions($mentions);
-            preg_match_all('/\[([^\]]*)\]/', $tweet->text, $matches);
-            $category = str_replace(']', '', str_replace('[', '', $matches[0]))[0];
-            $myTweet->setCategory($category);
-            $hashtags = [];
-            foreach ($tweet->entities->hashtags as $hashtag) {
-                array_push($hashtags, $hashtag->text);
-            }
-            $myTweet->setHashtags($hashtags);
-            $myTweet->setTitle(str_replace('[' . $category . '] ', '', $tweet->text));
-            $myTweet->setUrl($tweet->entities->urls[0]->expanded_url);
-            if ($this->getDoctrine()->getRepository('AppBundle:Tweet')->findOneBy(['imageUrl' => $myTweet->getImageUrl()]) == null) {
-                $this->getDoctrine()->getManager()->persist($myTweet);
-                $this->getDoctrine()->getManager()->flush();
+        if($request->cookies->get('daily_req') == null){
+            setcookie('daily_req', 1, time()+86400, "/");
+            $tweets = $this->get('app.twitter')->get('statuses/user_timeline', [
+                'screen_name' => 'grands_enfants',
+                'exclude_replies' => true
+            ]);
+            foreach ($tweets as $tweet) {
+                $myTweet = new Tweet();
+                $myTweet->setCreatedAt(new \DateTime($tweet->created_at));
+                $myTweet->setLikes($tweet->favorite_count);
+                $myTweet->setImageUrl($tweet->entities->media[0]->media_url);
+                $mentions = [];
+                foreach ($tweet->entities->user_mentions as $mention) {
+                    array_push($mentions, $mention->screen_name);
+                }
+                $myTweet->setMentions($mentions);
+                preg_match_all('/\[([^\]]*)\]/', $tweet->text, $matches);
+                $category = str_replace(']', '', str_replace('[', '', $matches[0]))[0];
+                $myTweet->setCategory($category);
+                $hashtags = [];
+                foreach ($tweet->entities->hashtags as $hashtag) {
+                    array_push($hashtags, $hashtag->text);
+                }
+                $myTweet->setHashtags($hashtags);
+                $myTweet->setTitle(str_replace('[' . $category . '] ', '', $tweet->text));
+                $myTweet->setUrl($tweet->entities->urls[0]->expanded_url);
+                if ($this->getDoctrine()->getRepository('AppBundle:Tweet')->findOneBy(['imageUrl' => $myTweet->getImageUrl()]) == null) {
+                    $this->getDoctrine()->getManager()->persist($myTweet);
+                    $this->getDoctrine()->getManager()->flush();
+                }
             }
         }
+
         return $this->render('default/index.html.twig', ['subtitle' => 'Last tweets', 'tweets' => $this->getDoctrine()->getRepository('AppBundle:Tweet')->findAll()]);
 
 
